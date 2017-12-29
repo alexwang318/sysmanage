c851d175f85289d2ac55aa4760d29196cf9a3953
alex <alexwang318@gmail.com>
2017-12-29 14:19:16 +0800

    Add user-group domain

---
+++ src/main/java/com/oracle/sp/dao/security/UserGroupMapper.java
@@ -0,0 +1,16 @@
+package com.oracle.sp.dao.security;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.oracle.sp.domain.GroupDO;
+
+public interface UserGroupMapper {
+	void insert(@Param("userName") String userName, @Param("groupName") String groupName);
+	
+	void deleteByUserName(@Param("userName") String userName);
+	
+	//Get all role names for a user.
+	List<GroupDO> selectGroups4User(@Param("userName") String userName);
+}
